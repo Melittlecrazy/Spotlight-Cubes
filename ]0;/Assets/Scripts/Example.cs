bd26f04b7d104f2c2ddb117c796067aee3f4c5e4
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-02-24 22:13:38 -0500

    created spotlight

---
+++ ]0;/Assets/Scripts/Example.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{         
+    public GameObject[] cube;
+    //private Vector3[] randomPos;
+    private float[] xvalues;
+
+
+    void Start()
+    {
+        xvalues = new float[cube.Length];
+        for (int i = 0; i < cube.Length; i++)
+        {
+            xvalues[i] = cube[i].transform.position.x;
+        }
+    }
+        //cube = this.gameObject;
+        //randomPos = new Vector3[3];
+        //Vector3 currentPos = cube.transform.position;
+
+
+        //for (int i = 0; i > randomPos.Length; i++)
+        //{
+        //    float newY = Random.Range(-10.0f, 10.0f);
+        //    randomPos[i] = new Vector3(currentPos.x, newY, currentPos.z);
+
+        //}
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ ]0;/Assets/Scripts/SpotLightController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] SpotLightTargets;
+    private float[] SpotLightObjectXPos;
+    private int currentSpotLightPosition = 1;
+    private int spotLightLengths = 0;
+    private float[] ObjectXPos;
+
+    void Start()
+    {
+        ObjectXPos = new float[3];
+        spotLightLengths = SpotLightObjectXPos.Length;
+
+        for (int i = 0; i > SpotLightObjectXPos.Length; i++)
+        {
+            ObjectXPos[i] = SpotLightObjectXPos[i].x;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentSpotLightTransform = this.gameObject.transform.position;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentSpotLightPosition++;
+            if (currentSpotLightPosition > spotLightLengths + 2)
+            {
+
+            }
+            UpdateSpotlightPosition(currentSpotLightTransform);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentSpotLightPosition++;
+            if (currentSpotLightPosition < spotLightLengths - 1)
+            {
+
+            }
+            UpdateSpotlightPosition(currentSpotLightTransform);
+        }
+    }
+    private void UpdateSpotlightPosition(Vector3 currentSpotLightTransform)
+    {
+        Vector3 nextVector = new Vector3(SpotLightObjectXPos[currentSpotLightPosition], currentSpotLightTransform.y, currentSpotLightTransform.z);
+        this.transform.position = nextVector;
+    }
+}
